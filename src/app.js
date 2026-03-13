ddb1afc79a055020e7ecaf46dd6dc5b006ccb8fc
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-13 12:16:02 +0530

    Enhance EJS templates to support multiple messages and add new views for scriptlet and unescaped content

--- src/app.js
+++ src/app.js
@@ -6,20 +6,22 @@ app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//   res.render("index", { message: "Hello from EJS!" });
-// });
-
-// app.get("/",(req,res)=>{
-//     res.render("index",{messages:[
-//         "Hello from EJS!",
-//         "Welcome to the EJS tutorial.",
-//         "EJS is a simple templating language that lets you generate HTML markup with plain JavaScript."
-//     ]})
-// })
-
 app.get("/", (req, res) => {
+  res.render("index", { message: "Hello from EJS!" });
+});
+
+app.get("/scriptlet", (req, res) => {
   res.render("index", {
+    messages: [
+      "Hello from EJS!",
+      "Welcome to the EJS tutorial.",
+      "EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.",
+    ],
+  });
+});
+
+app.get("/unescaped", (req, res) => {
+  res.render("unEscaped", {
     messages: [
       `<h1>Hello from EJS!</h1>`,
       `<p>Welcome to the EJS tutorial.</p>`,
