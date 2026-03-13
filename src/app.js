fe0d09574677ed5648fe26b2a4518f683b6aa209
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-13 11:48:39 +0530

    Add EJS view rendering and Morgan logging middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { message: "Hello from EJS!" });
+});
 
 module.exports = app;
