942b773a023b2862a27427d59f88522c4aed915c
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-13 12:04:56 +0530

    Update index route to render multiple messages in EJS template

--- src/app.js
+++ src/app.js
@@ -6,8 +6,18 @@ app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index", { message: "Hello from EJS!" });
-});
+// app.get("/", (req, res) => {
+//   res.render("index", { message: "Hello from EJS!" });
+// });
+
+app.get("/",(req,res)=>{
+    res.render("index",{messages:[
+        "Hello from EJS!",
+        "Welcome to the EJS tutorial.",
+        "EJS is a simple templating language that lets you generate HTML markup with plain JavaScript."
+    ]})
+})
+
+
 
 module.exports = app;
